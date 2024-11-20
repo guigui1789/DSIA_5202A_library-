fd36b593ae8c20eb48209f4d56994b88e23f8fde
guigui1789 <138137531+guigui1789@users.noreply.github.com>
2024-11-20 04:12:22 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from models.user import User
-from schemas.user_schema import UserCreate, User as UserSchema
-from database import SessionLocal, engine, Base
+from .models.user import User
+from .schemas.user_schema import UserCreate, User as UserSchema
+from .database import SessionLocal, engine, Base
 
 app = FastAPI()
 
