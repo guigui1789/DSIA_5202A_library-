95eb2f56b28e2fcfa1218ef66dd57ce3fd76679b
guigui1789 <138137531+guigui1789@users.noreply.github.com>
2024-11-20 04:10:21 +0100

    Update database.py

--- app/database/database.py
+++ app/database/database.py
@@ -2,16 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL de connexion à la base de données PostgreSQL
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db/library_db"
 
-# Création de l'engine de base de données
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 
-# Session pour interagir avec la base de données
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base pour les modèles déclaratifs
 Base = declarative_base()
